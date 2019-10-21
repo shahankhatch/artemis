6dc5dfe4a45721f209bda9828c25614b062b6f71
Shahan Khatchadourian <shahan.k.code@gmail.com>
2019-10-20 21:29:30 -0400

    Greatly extending the approach to connecting Handel through libp2p

--- networking/p2p/src/test/java/tech/pegasys/artemis/networking/handel/HandelP2PNetworkTest.java
+++ networking/p2p/src/test/java/tech/pegasys/artemis/networking/handel/HandelP2PNetworkTest.java
@@ -8,10 +8,19 @@ import io.libp2p.core.crypto.KEY_TYPE;
 import io.libp2p.core.crypto.KeyKt;
 import io.libp2p.core.crypto.PrivKey;
 import io.libp2p.core.multiformats.Multiaddr;
+import io.libp2p.etc.AttributesKt;
 import io.libp2p.protocol.PingController;
+import io.libp2p.tools.TestChannel;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -36,6 +45,7 @@ import tech.pegasys.artemis.util.SSZTypes.Bitlist;
 import tech.pegasys.artemis.util.bls.BLSKeyPair;
 import tech.pegasys.artemis.util.bls.BLSPublicKey;
 import tech.pegasys.artemis.util.bls.BLSSignature;
+import tech.pegasys.artemis.util.bls.BLSVerify;
 import tech.pegasys.artemis.util.mikuli.KeyPair;
 import tech.pegasys.artemis.util.mikuli.PublicKey;
 import tech.pegasys.artemis.util.mikuli.Signature;
@@ -215,15 +225,10 @@ public class HandelP2PNetworkTest {
 
     // isolate and start host node
     HandelP2PNetwork handelServer = nodes.get(0);
-    nodes.remove(0);
-    handelServer.getHost()
-        .start().thenAccept(i -> {
-      testLogger.debug("Node 0 (host node) started on port " + startPort);
-    }).get(45, TimeUnit.SECONDS);
 
 //    Executor executor = Executors.newFixedThreadPool(10);
 
-    // start peer nodes
+    // define startup of peer nodes
     List<CompletableFuture<?>> collect = nodes.stream().map(
         n -> {
           testLogger.debug("Attempting to start node on port " + n.getConfig().getListenPort());
@@ -233,45 +238,54 @@ public class HandelP2PNetworkTest {
         }
     ).collect(Collectors.toList());
 
-    CompletableFuture<Void> voidCompletableFuture = CompletableFuture
-        .allOf(collect.toArray(new CompletableFuture[0]));
+    // execute all the startup futures
+    CompletableFuture.allOf(collect.toArray(new CompletableFuture[0])).join();
 
-    voidCompletableFuture.join();
-
-    String peer =
+    // define a single node as the central server
+    String centralServer =
         "/ip4/127.0.0.1/tcp/" + handelServer.getConfig().getListenPort() + "/p2p/"
             + handelServer.getHost().getPeerId();
 
-    java.util.stream.Stream<CompletableFuture<Connection>> completableFutureStream = nodes.stream()
-        .parallel().map(n -> {
+    // define connection of all peers to central server
+    List<CompletableFuture<Connection>> completableFutureStream = nodes
+        .subList(1, nodes.size()).stream()
+        .map(n -> {
           testLogger.debug("Connecting from:" + n.getConfig().getListenPort());
-          return n
+          CompletableFuture<Connection> connectionCompletableFuture = n
               .getHost().getNetwork().connect(handelServer.getHost().getPeerId(),
                   new Multiaddr("/ip4/127.0.0.1/tcp/" + handelServer.getConfig().getListenPort()))
-              //          .connect(peer)
               .thenApply(fff -> {
+                n.setConnection(fff);
                 testLogger.debug("Connected successfully:" + fff);
                 return fff;
               });
+          return connectionCompletableFuture;
 
 //            .thenApply(it -> it.getMuxerSession().createStream(
 //            Multistream.create(new Ping()).toStreamHandler()))
 
-        });
+        }).collect(Collectors.toList());
 
+    // wait for connections to complete
     CompletableFuture.allOf(
-        completableFutureStream.collect(Collectors.toList()).toArray(new CompletableFuture[0]))
+        completableFutureStream.toArray(new CompletableFuture[0]))
         .join();
 
     testLogger.debug("Completed connections");
 
-    // create a committee based on peers
+    // create a committee of peer validators
+    // populate the committee with a new HandelParticipant for each peer
     HandelCommittee hc = new HandelCommittee(
         handelServer.getConfig().getPrivateKey().get().publicKey(),
         Bytes32.ZERO, nodes.stream().map(n -> {
-      // TODO: change HP to be an interface with an implementation that wraps current libp2p abstraction
-      return new HandelParticipant(nodes.indexOf(n),
+      HandelParticipant handelParticipant = new HandelParticipant(
           n.getConfig().getPrivateKey().get().publicKey());
+      KeyPair randomKeyPair = KeyPair.random();
+      BLSKeyPair kkp = new BLSKeyPair(randomKeyPair);
+      handelParticipant.setKp(randomKeyPair);
+      handelParticipant.setBlsKeyPair(kkp);
+      handelParticipant.setNetwork(n);
+      return handelParticipant;
     }).collect(Collectors.toList()));
 
     Assertions.assertTrue(hc.getParticipants().size() > 0);
@@ -281,24 +295,33 @@ public class HandelP2PNetworkTest {
     // assume that there is a single common attestation data that all peers want to certify
     AttestationData attestationData = DataStructureUtil.randomAttestationData();
 
-    // prepare initial state of all nodes
-    testLogger.debug("Setting up initial committee participants");
-    hc.getParticipants().forEach(p -> {
-      KeyPair randomKeyPair = KeyPair.random();
-      BLSKeyPair kkp = new BLSKeyPair(randomKeyPair);
-
-      // get current participant's aggregation state
-      // currently creating it anew, signing zero block
-      HandelAggregation ha = new HandelAggregation();
-      ha.setHash(Bytes32.ZERO);
-      ha.setSigners(new Bitlist(hc.getParticipants().size(), hc.getParticipants().size()));
-      ha.getSigners().setBit(hc.getSortedParticipantKeys().indexOf(p.getKey())); // set our own bit
-      ha.setSignature(BLSSignature.sign(kkp, Bytes32.ZERO.reverse(), Bytes32.ZERO.reverse()));
-      ha.setPubkey(kkp.getPublicKey());
-    });
+    // get the key by which to sign all outgoing aggregations
+    // that is, the connections are initiaded by the peers to the central node
+    // and the
+    BLSKeyPair serverkp = new BLSKeyPair(hc.getParticipants().get(0).getKp());
 
     testLogger.debug("Activate each participant");
-    hc.getParticipants().forEach(p -> {
+    hc.getParticipants().subList(1, hc.getParticipants().size()).forEach(p -> {
+
+      // initialize a node's simulated incoming connections
+      List<HandelAggregation> incomingAggregations = p.getIncomingAggregations();
+      for (int j = 0; j < 10; j++) {
+        HandelAggregation ha = new HandelAggregation();
+        ha.setHash(Bytes32.ZERO);
+        ha.setSigners(new Bitlist(hc.getSortedParticipantKeys().size(),
+            hc.getSortedParticipantKeys().size()));
+        ha.getSigners()
+            .setBit(hc.getSortedParticipantKeys().indexOf(p.getKey())); // set our own bit
+
+        ha.setSignature(
+            BLSSignature.sign(serverkp, Bytes32.ZERO.reverse(), Bytes32.ZERO.reverse()));
+        ha.setPubkey(serverkp.getPublicKey());
+        incomingAggregations.add(ha);
+      }
+
+      testLogger.debug("Added simulated incoming aggregtions");
+      Assertions.assertEquals(10, incomingAggregations.size(),
+          "Incoming aggregations were not properly formed.");
 
       Map<Bytes32, HandelAggregation> mapHashToAggr = p.getMapHashToAggr();
 
@@ -309,29 +332,76 @@ public class HandelP2PNetworkTest {
 //      SignatureAndPublicKey attestationSignatureAndPubkey = BLS12381
 //          .sign(kp2, attestationData.hash_tree_root().toArray(), 0);
 
-      // handling incoming
-      p.getIncomingAggregations().forEach(incomingAggr -> {
+      testLogger.debug("Starting to aggregate incoming");
+
+      // handle incoming
+
+      testLogger.debug("Incoming aggregations - presort: " + incomingAggregations.toString());
+
+      // sort aggregations by their BLSPubKey order
+      Arrays.sort(incomingAggregations.toArray(new HandelAggregation[0]),
+          (Comparator<HandelAggregation>) (ths, other) -> {
+            return
+                HandelCommittee.compare(
+                    HandelCommittee.determineVPByBLSPubKey(hc.getParticipants(), Bytes32.ZERO)
+                        .stream().filter(participant -> {
+                      return ths.getPubkey().equals(participant.getBlsKeyPair().getPublicKey());
+                    }).collect(Collectors.toList()).get(0).getBlsKeyPair().getPublicKey().toBytes()
+                        .toArray(),
+                    HandelCommittee.determineVPByBLSPubKey(hc.getParticipants(), Bytes32.ZERO)
+                        .stream().filter(participant -> {
+                      return other.getPubkey().equals(participant.getBlsKeyPair().getPublicKey());
+                    }).collect(Collectors.toList()).get(0).getBlsKeyPair().getPublicKey().toBytes()
+                        .toArray());
+          });
+
+      testLogger.debug("Incoming aggregations - postsort: " + incomingAggregations.toString());
+
+      // move incoming aggregations
+      p.setIncomingAggregations(new ArrayList<>());
+
+      incomingAggregations.forEach(incomingAggr -> {
         // set initial value to first encounter
         if (!mapHashToAggr.containsKey(incomingAggr.getHash())) {
           mapHashToAggr.put(incomingAggr.getHash(), incomingAggr);
           return;
         }
 
+        // verify the signature of the incoming aggregation is valid
+        boolean aggrVerification = BLSVerify
+            .bls_verify(incomingAggr.getPubkey(), incomingAggr.getHash(),
+                incomingAggr.getSignature(), Bytes32.ZERO);
+
+        if (!aggrVerification) {
+          testLogger.debug("Verification of incoming aggr failed");
+        }
+        testLogger.debug("Verification of incoming aggr passed");
+
         // check that we should aggregate this signature
 
         // signataure bitlists don't have much functionality i.e., there isn't an xor op
         // so it's easiest to just iterate over bitlist and use individual participant's attestation signature
 
+        // get the current aggregation state for this hash
         PublicKey aggrKey = mapHashToAggr.get(incomingAggr.getHash()).getPubkey().getPublicKey();
         Bitlist aggrSigners = mapHashToAggr.get(incomingAggr.getHash()).getSigners().copy();
         BLSSignature combine = mapHashToAggr.get(incomingAggr.getHash()).getSignature();
 
+        // TODO: mark location for optimizations that order signers
+        // iterate over signers and add each to the current hash signature if not present
         for (int i = 0; i < incomingAggr.getSigners().getMaxSize(); i++) {
-          if (i == hc.getSortedParticipantKeys().indexOf(p.getKey())) {
-            testLogger.debug("Skiping our own bit:" + i);
+          if (i == HandelCommittee.determineVPByBLSPubKey(hc.getParticipants(), Bytes32.ZERO)
+              .indexOf(p)) {
+            testLogger.debug("Skipping our own bit:" + i);
             // skip our own bit
             continue;
           }
+          // skip signers that are already part of the signature
+          if (aggrSigners.getBit(i) != 0) {
+            continue;
+          }
+          // TODO: mark location for optimizations that skip certain signers
+
           aggrSigners.setBit(i);
           combine = new BLSSignature(incomingAggr.getSignature().getSignature()
               .combine(combine.getSignature()));
@@ -339,6 +409,7 @@ public class HandelP2PNetworkTest {
               .aggregate(Arrays.asList(aggrKey, incomingAggr.getPubkey().getPublicKey()));
         }
 
+        // update the current state for that hash
         HandelAggregation ha = mapHashToAggr.get(incomingAggr.getHash());
         ha.setHash(incomingAggr.getHash());
         ha.setSigners(aggrSigners);
@@ -346,9 +417,23 @@ public class HandelP2PNetworkTest {
         ha.setSignature(combine);
 
       });
+      // completed processing incoming edges
+
+      // now send outgoing
+      Connection connection = p.getNetwork().getConnection();
+      ChannelFuture channelFuture = connection.getNettyChannel().writeAndFlush(Unpooled.wrappedBuffer("Hello World from 1".getBytes()));
+//      channelFuture.channel().writeAndFlush(Unpooled.wrappedBuffer("Hello World from 2".getBytes()));
+      try {
+        channelFuture.get(5, TimeUnit.SECONDS);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      } catch (ExecutionException e) {
+        e.printStackTrace();
+      } catch (TimeoutException e) {
+        e.printStackTrace();
+      }
 
     });
-
 
     testLogger.debug("Completed activation of nodes");
 
