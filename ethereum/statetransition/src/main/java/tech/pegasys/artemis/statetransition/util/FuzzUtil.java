ee42bb504f86564f84c5252a41a0311c7fb06269
Shahan Khatchadourian <shahan.k.code@gmail.com>
2020-01-05 17:15:06 -0500

    moving along with byte array parameters and return types

--- ethereum/statetransition/src/main/java/tech/pegasys/artemis/statetransition/util/FuzzUtil.java
+++ ethereum/statetransition/src/main/java/tech/pegasys/artemis/statetransition/util/FuzzUtil.java
@@ -2,39 +2,50 @@ package tech.pegasys.artemis.statetransition.util;
 
 import static tech.pegasys.artemis.statetransition.util.BlockProcessorUtil.process_attestations;
 
+import com.google.common.io.ByteStreams;
+import com.google.common.primitives.UnsignedLong;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.tuweni.bytes.Bytes;
 import tech.pegasys.artemis.datastructures.operations.Attestation;
 import tech.pegasys.artemis.datastructures.state.BeaconState;
 import tech.pegasys.artemis.datastructures.state.BeaconStateWithCache;
+import tech.pegasys.artemis.datastructures.state.PendingAttestation;
 import tech.pegasys.artemis.datastructures.util.AttestationUtil;
 import tech.pegasys.artemis.datastructures.util.BeaconStateUtil;
 import tech.pegasys.artemis.datastructures.util.DataStructureUtil;
+import tech.pegasys.artemis.datastructures.util.SimpleOffsetSerializer;
 
 public class FuzzUtil {
-//  Logger logger = LogManager.getLogger();
 
   public void sayHello(String h) {
     System.out.println("hi:" + h + ":");
   }
 
   public static void main(String[] args) {
-    FuzzUtil h = new FuzzUtil();
+    final FuzzUtil h = new FuzzUtil();
     h.sayHello("here");
     try {
       h.doblock();
     } catch (BlockProcessingException e) {
-      System.out.println("error");
-      System.err.println("e:" + e.getMessage());
+//      System.out.println("error");
+//      System.err.println("e:" + e.getMessage());
     }
+    h.doblock2();
+    boolean b = FuzzUtil.testAttestation(new byte[0], new byte[0]);
+    System.out.println("bool:" + b);
   }
 
   private void doblock() throws BlockProcessingException {
-    BeaconState beaconState = new BeaconState();
-    Attestation attestation = DataStructureUtil.randomAttestation(0);
-    ArrayList<Attestation> attestations = new ArrayList<>();
+    final BeaconState beaconState = new BeaconState();
+    final Attestation attestation = DataStructureUtil.randomAttestation(0);
+    attestation.setData(attestation.getData().withIndex(UnsignedLong.ZERO));
+//    PendingAttestation pendingAttestation = DataStructureUtil.randomPendingAttestation(0);
+
+    final ArrayList<Attestation> attestations = new ArrayList<>();
     attestations.add(attestation);
 
     System.out.println("b_before:" + beaconState.getSlot());
@@ -42,8 +53,28 @@ public class FuzzUtil {
     System.out.println("b_after:" + beaconState.getSlot());
   }
 
-  public static boolean testAttestation(byte[] in) {
+  private void doblock2() {
+    final Bytes inData = Bytes.wrap(new byte[0]);
+    try {
+      final BeaconStateWithCache state =
+          BeaconStateWithCache.fromBeaconState(
+              SimpleOffsetSerializer.deserialize(inData, BeaconState.class));
+      System.out.println("statet:" + state.getSlot());
+      final BeaconState result = null;
+      SimpleOffsetSerializer.serialize(result).toArrayUnsafe();
+    } catch (Exception e) {
+      System.err.println("state error");
+    }
+  }
+
+  public static boolean testAttestation(final byte[] beaconstate, final byte[] attestation) {
+    byte[] ret = testAttestationRetState(beaconstate, attestation);
+    System.out.println("retsize:" + ret.length);
     return true;
+  }
+
+  public static byte[] testAttestationRetState(final byte[] beaconstate, final byte[] attestation) {
+    return new byte[0];
   }
 
 
